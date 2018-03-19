c7c3324793aadd16ca8c7c47d0394fbcd5149336
neewy <n.yushkevich@hotmail.com>
2018-03-19 17:31:24 +0300

    Add symlinks for Makefile
    Signed-off-by: neewy <n.yushkevich@hotmail.com>

--- scripts/generate_symlinks.py
+++ scripts/generate_symlinks.py
@@ -2,12 +2,15 @@ import os
 
 rst_files = {} # Where all original rst files are stored
 langs = {} # All the languages of the project
+makefile = None
 
 # Scan for .rst files
 for root, dirs, files in os.walk("../iroha/docs/source"):
     for file in files:
         if file.endswith(".rst"):
             rst_files[file] = os.path.join(root, file)
+        if file.endswith("Makefile"):
+            makefile = os.path.join(root, file)
 
 # Find all languages
 for root, dirs, files in os.walk("../docs"):
@@ -18,3 +21,4 @@ for root, dirs, files in os.walk("../docs"):
 for lang in langs:
     for rst in rst_files:
         os.symlink("../" + rst_files[rst], langs[lang] + "/" + rst)
+    os.symlink("../" + makefile, langs[lang] + "/" + "Makefile")
